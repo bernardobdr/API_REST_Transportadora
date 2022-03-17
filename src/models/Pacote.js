a5abcf8e0332723b64f6605e6f002146d70c9b01
bernardobdr <bernardosilvabdr@gmail.com>
2022-03-16 23:13:23 -0300

    Criando a classe pacote

---
+++ src/models/Pacote.js
@@ -0,0 +1,11 @@
+class Pacote {
+    constructor(id, cliente_id, frete, peso, dimensao){
+        this.id = id
+        this.cliente_id = cliente_id
+        this.frete = frete
+        this.peso = peso
+        this.dimensao = dimensao
+    }
+}
+
+export default Pacote
