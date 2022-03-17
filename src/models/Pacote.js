6afe2ef38d88a0cd4c0a34dcadb340951f994531
bernardobdr <bernardosilvabdr@gmail.com>
2022-03-17 00:08:39 -0300

    criando tabelas e inserindo dados

--- src/models/Pacote.js
+++ src/models/Pacote.js
@@ -1,10 +1,12 @@
 class Pacote {
-    constructor(id, cliente_id, frete, peso, dimensao){
+    constructor(id, cliente_id, frete, peso, largura, altura, comprimento){
         this.id = id
         this.cliente_id = cliente_id
         this.frete = frete
         this.peso = peso
-        this.dimensao = dimensao
+        this.largura = largura
+        this.altura = altura
+        this.comprimento = comprimento
     }
 }
 
