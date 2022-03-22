88176fb04657786ac114e8919e9149fdb610d948
bernardobdr <bernardosilvabdr@gmail.com>
2022-03-22 00:20:40 -0300

    debugando pacote-controller

--- src/controllers/pacote-controller.js
+++ src/controllers/pacote-controller.js
@@ -1,85 +1,84 @@
 import Pacote from "../models/Pacote.js";
-import PacoteDAO from "../DAO/pacote-DAO.js"
+import PacoteDAO from "../DAO/pacote-DAO.js";
 
-const pacoteController = (app, bd)=>{
-    const pacoteDAO = new PacoteDAO(bd)
+function pacoteController(app, bd) {
+    const pacoteDAO = new PacoteDAO(bd);
 
-    app.get('/pacote', (req, res)=>{
-        pacoteDAO.pegaTodosPacotes()
-        .then((resposta)=>{
-            res.status(302).json(resposta)
-        })
-        .catch((erro)=>{
-            res.status(404).json(erro)
-        })
-    })
-
-    app.get('/pacote/id/:id', (req, res)=>{
-        const id = req.params.id
-
-        pacoteDAO.pegaUmPacote(id)
-        .then((resposta)=>{
-            res.status(302).json(resposta)
-        })
-        .catch((erro)=>{
-            res.status(404).json(erro)
-        })
-    })
-
-    app.post('/pacote', (req, res)=>{
-        const body = req.body
-        try {
-            const novoPacote = new Pacote(body.ID, body.CLIENTE_ID, body.FRETE, body.PESO, body.LARGURA, body.ALTURA, body.COMPRIMENTO)
-            
-            pacoteDAO.inserePacote(novoPacote)
-            .then((resposta)=>{
-                res.status(201).json(resposta)
-            })
-            .catch((erro)=>{
-                res.status(400).json(erro)
-            })
-        } catch (error) {
-            res.json({
-                "msg": error.message,
-                "erro": true
-            })
-        }
-    })
-
-    app.delete('/pacote/id/:id', (req, res)=>{
-        const id = req.params.id
-
-        pacoteDAO.deletaPacote(id)
-        .then((resposta)=>{
-            res.status(200).json(resposta)
-        })
-        .catch((erro)=>{
-            res.status(404).json(erro)
-        })
-    })
-
-    app.put('/pacote/id/:id', (req, res)=>{
-        const id = req.params.id
-
-        const body = req.body
+    app.get('/pacote', async (req, res) => {
 
         try {
-            const pacoteAtualizado = new Pacote(body.ID, body.NOME_COMPLETO, body.CPF, body.TELEFONE, body.EMAIL, body.PEDIDOS_ID)
-
-            pacoteDAO.atualizaPacote(id, pacoteAtualizado)
-            .then((resposta)=>{
-                res.status(200).json(resposta)
-            })
-            .catch((erro)=>{
-                res.status(404).json(erro)
-            })
-        } catch (error) {
-            res.json({
-                "msg": error.message,
-                "erro": true
-            })
+            res.json(await pacoteDAO.pegaTodosPacotes());
         }
-    })
+        catch (error) {
+            res.status(400).json(error);
+        }
+    
+    });
 }
+    app.get('/pacote/id/:id', async (req, res) => {
+        const id = req.params.id;
 
-export default pacoteController
+        try {
+            await pacoteDAO._verificaId(id);
+            const pacote = await pacoteDAO.pegaUmPacote(id);
+            res.status(302).json(pacote);
+        }
+        catch (error) {
+            res.status(404).json(error.message);
+        }
+
+        app.post('/pacote', (req, res) => {
+            const body = req.body;
+            try {
+                const novoPacote = new Pacote(body.ID, body.CLIENTE_ID, body.FRETE, body.PESO, body.LARGURA, body.ALTURA, body.COMPRIMENTO);
+
+                res.status(201).json(await pacoteDAO.inserePacote(novoPacote));
+
+
+            } catch (error) {
+                res.status(400).json(error);
+
+                res.json({
+                    'msg': error.message,
+                    'erro': true
+                });
+            }
+        });
+        app.delete('/pacote/id/:id', async (req, res) => {
+            const id = req.params.id;
+            try {
+                await pacoteDAO._verificaId(id);
+                const delUsuario = await pacoteDAO.deletaPacote(id);
+                res.status(201).json(delPacote);
+            }
+            catch (error) {
+                res.status(400).json(error.message);
+            }
+        });
+
+        app.put('/pacote/id/:id', async (req, res) => {
+            const id = req.params.id;
+
+            const body = req.body;
+
+            try {
+                const pacoteAtualizado = new Pacote(body.ID, body.CLIENTE_ID, body.FRETE, body.PESO, body.LARGURA, body.ALTURA, body.COMPRIMENTO);
+
+                await pacoteDAO._verificaId(id);
+                const attPacote = await pacoteDAO.atualizaPacote(id, pacoteAtualizado);
+                res.status(200).json(attPacote);
+            }
+            catch (error) {
+
+                res.status(404).json(error.message);
+
+                res.json({
+                    'msg': error.message,
+                    'erro': true
+                });
+            }
+        });
+    });
+
+
+    export default pacoteController
