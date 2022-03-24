7290574e8af2dfc5ec6e1d8f7bb9b484ad580b1f
bernardobdr <bernardosilvabdr@gmail.com>
2022-03-23 22:34:47 -0300

    finalizando alterações

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
-import express from "express";
-import cors from "cors";
-import pacoteController from "./controllers/pacote-controller.js";
-import generalMiddleware from "./middleware/general-middleware.js";
-import database from "./database/sqlite-db.js";
+import express from "express"
+import cors from "cors"
+import pacoteController from "./controllers/pacote-controller.js"
+import generalMiddleware from "./middleware/general-middleware.js"
+import database from "./database/sqlite-db.js"
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-generalMiddleware(app);
+generalMiddleware(app)
 
-pacoteController(app, database);
+pacoteController(app, database)
 
-export default app;
+export default app
